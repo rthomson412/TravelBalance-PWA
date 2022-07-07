51eae72ab9c542aabab61644f53f2872502ea1c9
Ross Thomson <rthomson412@gmail.com>
2022-07-07 08:22:40 -0400

    fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,9 +20,9 @@ const FILES_TO_CACHE = [
   "./icons/icon-512x512.png"
 ];
 
-// Cache resources
+
 self.addEventListener("install", function(event) {
-  // Perform install steps
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       console.log('installing cache : ' + CACHE_NAME)
@@ -31,15 +31,15 @@ self.addEventListener("install", function(event) {
   );
 });
 
-// Respond with cached resources
+
 self.addEventListener("fetch", function(event) {
-  // cache all get requests to /api routes
+
   if (event.request.url.includes("/api/")) {
     event.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(event.request)
           .then(response => {
-            // If the response was good, clone it and store it in the cache.
+ 
             if (response.status === 200) {
               cache.put(event.request.url, response.clone());
             }
@@ -47,7 +47,7 @@ self.addEventListener("fetch", function(event) {
             return response;
           })
           .catch(err => {
-            // Network request failed, try to get it from the cache.
+
             return cache.match(event.request);
           });
       }).catch(err => console.log(err))
@@ -62,7 +62,7 @@ self.addEventListener("fetch", function(event) {
         if (response) {
           return response;
         } else if (event.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
+
           return caches.match("/");
         }
       });
@@ -70,16 +70,14 @@ self.addEventListener("fetch", function(event) {
   );
 });
 
-// Delete outdated caches
+
 self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create white list
+
       let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
       })
-      // add current cache name to white list
       cacheKeeplist.push(CACHE_NAME);
 
       return Promise.all(keyList.map(function (key, i) {
